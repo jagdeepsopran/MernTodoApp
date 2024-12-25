cde848b628b18738ffd7434005a464a08bd7e89a
amitshankhwar <amit18shankhwar@gmail.com>
2024-12-25 11:26:02 +0530

    backend done

--- Backend/controllers/todoControllers.js
+++ Backend/controllers/todoControllers.js
@@ -24,6 +24,86 @@ export async function createTodoController(req, res) {
   }
 }
 
-export async function updateTodoController() {}
-export async function deleteTodoController() {}
-export async function getAllTodoController() {}
+export async function updateTodoController(req, res) {
+  try {
+    const todoId = req.params.id;
+
+    // console.log(todoId);
+
+    const { title, description } = req.body;
+
+    if (!title || !description) {
+      return res
+        .status(400)
+        .json({ message: "Please provide a title and description" });
+    }
+
+    const updatedTodo = await Todo.findOneAndUpdate(
+      { _id: todoId },
+      { title, description },
+      { new: true }
+    );
+
+    if (!updatedTodo) {
+      return res.status(400).json({
+        success: false,
+        message: "failed in updating todo",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "todo updated successfully",
+      updatedTodo,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error,
+    });
+  }
+}
+export async function deleteTodoController(req, res) {
+  try {
+    const todoId = req.params.id;
+
+    const deletedTodoItem = await Todo.findByIdAndDelete(todoId);
+
+    if (!deletedTodoItem) {
+      return res.status(400).json({
+        success: false,
+        message: "failed in deleting todo item",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "todo deleted ssuccessfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error,
+    });
+  }
+}
+export async function getAllTodoController(req, res) {
+  try {
+    const allTodos = await Todo.find();
+
+    if (!allTodos) {
+      return res.status(400).json({
+        success: false,
+        message: "No todos found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "all todos send successfully",
+      allTodos,
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error });
+  }
+}
